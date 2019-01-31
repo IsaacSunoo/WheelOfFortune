3d6a2793a25db79652b982c81b1c72440cff10b3
Isaac Sunoo <isaacsunoo@gmail.com>
2019-01-31 13:35:30 -0700

    updated Wheel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@
 // Tell webpack to use a CSS file
 import './css/base.css';
 
+// Tell webpack to use these js files
+import './js/Board.js';
+import './js/Player.js';
+import './js/domUpdates';
+import './js/Game.js';
+import './js/Wheel.js';
+
+
 //  Tell webpack to use an image (link to it in index.html)
 import './images/turing-logo.png'
 
