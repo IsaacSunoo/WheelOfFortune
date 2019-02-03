d66d59aaf247547aedeacbf5dcca3d9f6e93477a
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-02-03 12:21:07 -0700

    Wheel stops at appropriate spot

--- src/js/domUpdates.js
+++ src/js/domUpdates.js
@@ -5,12 +5,47 @@ class domUpdates {
     }
 
     spinWheel() {
-        $(".spin-btn").on("click", () => {
-            $(".wheel").css("transform", "rotate(4188deg)");
-        });
+        var color = 1;
+        switch(color) {
+          case 1:
+          $(".spin-btn").on("click", () => {
+            $(".wheel").css("transform", "rotate(720deg)");
+              });
+          break;
+          case 2:
+          $(".spin-btn").on("click", () => {
+            $(".wheel").css("transform", "rotate(780deg)");
+              });
+          break;
+          case 3:
+          $(".spin-btn").on("click", () => {
+            $(".wheel").css("transform", "rotate(840deg)");
+              });
+          break;
+          case 4:
+          $(".spin-btn").on("click", () => {
+            $(".wheel").css("transform", "rotate(900deg)");
+              });
+          break;
+          case 5:
+          $(".spin-btn").on("click", () => {
+            $(".wheel").css("transform", "rotate(960deg)");
+              });
+          break;
+          case 6:
+          $(".spin-btn").on("click", () => {
+            $(".wheel").css("transform", "rotate(1020deg)");
+              });
+      }
     }
 
-}
+};
 
 export default domUpdates;
 
+// red 720
+// purple 780
+// blue 840
+// green 900
+// yellow 960
+// orange 1020
